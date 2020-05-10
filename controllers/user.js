464ce22296e93e5dd424af138c572df9380859c7
Robert Farrera <roberto.farrera@procesadigital.com>
2020-05-09 23:37:10 -0500

    métodos de usuario terminados. Falta manejar la sesión en la API

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,8 @@
 
 var validator = require('validator');
 var User = require('../models/user');
+var fs = require('fs');
+var path = require('path');
 
 var controller = {
 
@@ -38,7 +40,7 @@ var controller = {
 
       user.save((error, userStored)=>{
         if(error || !userStored){
-          return res.status(400).send({
+          return res.status(500).send({
             msg: 'No se puede completar el registro.',
             error
           });
@@ -52,7 +54,7 @@ var controller = {
       
     }else{
       
-      //Crear objeto con los datos
+      //Devolver error con datos inválidos
       return res.status(400).send({
         msg: 'Datos no válidos.',
         data: {
@@ -75,6 +77,194 @@ var controller = {
         }
       });
     }
+    
+  },
+
+  update:(req, res) => {
+     //obtener datos por POST
+     var params = req.body;
+     //validar datos con Validator
+     try {       
+      if(params.type === '3')
+        var valid_rfc = validator.matches(params.rfc, '[A-Z]{3}[0-9]{6}[A-Z-0-9]{3}'); 
+      else
+        var valid_rfc = validator.isEmpty(params.rfc);
+
+      var valid_name = !validator.isEmpty(params.name);
+      var valid_phone = validator.isMobilePhone(params.phone, 'es-MX');
+      var valid_pass = validator.isLength(params.pass, {min:8});
+       
+    } catch (error) {
+      return res.status(400).send({
+        msg: 'Faltan datos requeridos.'
+      });
+    }
+ 
+    if(valid_rfc && valid_name && valid_phone && valid_pass){
+      //Crear objeto con los datos a actualizar
+      var update = {
+        rfc: params.rfc,
+        name: params.name,
+        phone: params.phone,
+        pass: params.pass
+      };
+
+      User.findByIdAndUpdate(req.params.id,update, {new:true}, (error,user) => {
+        if(error){
+          return res.status(500).send({
+            msg:'Ocurrió un error al actualizar los datos.',
+            error
+          });
+        }
+
+        if(!user){
+          return res.status(404).send({
+            msg:'La cuenta de usuario que deseas actualizar no existe.',
+          });
+        }
+
+        return res.status(200).send({
+          msg:'Datos actualizados con éxito.',
+          user
+        });
+
+      });
+    }else{
+
+      //Devolver error con datos inválidos
+      return res.status(400).send({
+        msg: 'Datos no válidos.',
+        data: {
+          rfc: {
+            value: params.rfc,
+            valid: valid_rfc
+          },
+          name: {
+            value: params.name,
+            valid: valid_name
+          },
+          phone: {
+            value: params.phone,
+            valid: valid_phone
+          },
+          pass: {
+            value: params.pass,
+            valid: valid_pass
+          }
+        }
+      });
+    }
+
+  },
+
+  delete:(req, res) => {
+
+    User.findByIdAndDelete(req.params.id,(error,user)=>{
+      if(error){
+        return res.status(500).send({
+          msg:'Ha ocurrido un error.',
+          error
+        });
+      }
+
+      if(!user){
+        return res.status(400).send({
+          msg:'La cuenta de usuario no existe.'
+        });
+      }
+
+      return res.status(200).send({
+        msg:'La cuenta ha sido eliminada.',
+      });
+    });
+    
+  },
+
+  setPicture:(req, res) => {
+
+    if(!req.files.file0){
+      return res.status(400).send({
+        msg:'No se ha agregado ninguna imagen'
+      });
+    }
+
+    var img = req.files.file0;
+    var type = img.type.split('/')[1];
+
+    if(type != 'jpeg' && type != 'gif' && type != 'png'){
+      fs.unlink(img.path, (error)=>{
+        if(error){
+          return res.status(500).send({
+            msg:'Error con el sistema de archivos.',
+          });
+        }
+        return res.status(400).send({
+          msg:'No es un formato de imagen válido.',
+        });
+      });
+    }else if(img.size > 500*1024){
+      fs.unlink(img.path, (error)=>{
+        if(error){
+          return res.status(500).send({
+            msg:'Error con el sistema de archivos.',
+          });
+        }
+          
+        return res.status(400).send({
+          msg:'La imagen es demasiado grande: máximo 500kb.',
+        });
+      });
+    }else{
+
+      var file_name = img.path.split('\\')[2];
+    
+      User.findByIdAndUpdate(req.params.id,{image:file_name}, (error,user)=>{
+        if(error || !user){
+          return res.status(500).send({
+            msg:'Ocurrió un al actualizar la cuenta de usuario.',
+            error
+          });
+        }
+
+        if(user.image != null){
+          fs.unlink('uploads\\users\\'+user.image, (error)=>{
+            if(error){
+              return res.status(500).send({
+                msg:'Error con el sistema de archivos.',
+              });
+            }
+            return res.status(200).send({
+              msg:'Imagen actualizada.',
+              file_name
+            });
+          });
+        }else{
+          return res.status(200).send({
+            msg:'Imagen actualizada.',
+            file_name
+          });
+        }
+        
+      });
+
+    }
+
+    
+  },
+
+  getPicture:(req, res)=>{
+    var img_path = './uploads/users/'+req.params.img;
+
+    //verificar si el archivo existe
+    fs.exists(img_path,(exists)=>{
+      if(exists){
+        return res.sendFile(path.resolve(img_path));
+      }else{
+        return res.status(404).send({
+          msg:'La imagen solicitada no existe.'
+        });
+      }
+    });
     
   },
 
@@ -114,9 +304,11 @@ var controller = {
     });
 
   
-  }
-  
+  },
 
+  logout: (req,res) => {
+    //Cerrar sesión
+  }
 
 };
 
--- routes/routes.js
+++ routes/routes.js
@@ -4,9 +4,22 @@ var express = require('express');
 var UserController = require('../controllers/user');
 
 var router = express.Router();
+//miltiparty para subida de archivos
+var multiparty = require('connect-multiparty');
+var md_upload = multiparty({uploadDir:'./uploads/users'});
 
 //Rutas Usuario
 router.post('/register', UserController.save);
 router.post('/login', UserController.login);
+router.post('/logout', UserController.logout);
+
+router.put('/user/:id', UserController.update);
+router.delete('/user/:id', UserController.delete);
+router.post('/user/picture/:id',md_upload, UserController.setPicture);
+router.get('/user/picture/:img', UserController.getPicture);
+
+//Rutas para reportes
+
+
 
 module.exports = router;
